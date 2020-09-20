34b7641cb4a9b5189dbea24d0fca493229d3166d
vishnu <chilamakurvishnu@gmail.com>
2020-09-20 12:46:04 +0530

    /add-product, /product, / routes added

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ const app = express();
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res, next) => {
-  res.send("<h1>Hello</h1>");
+app.get("/add-product", (req, res, next) => {
+  res.send(`<form action='products' method='POST'>
+    <input type='text' name='title'/>
+    <button type='submit'>Add</button>
+</form>`);
 });
 
-app.listen(3000, function () {
-  console.log("server started at port : 3000");
+app.post("/products", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
 });
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.listen(3000, () => console.log(`Server stared at port : 3000`));
