73b176f3f0f5cadd063e14bfc49fda3361212f40
vishnu <chilamakurvishnu@gmail.com>
2020-09-20 15:27:31 +0530

    add-product.html, shop.html, 404.html created

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use("/admin", adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.send(`<h1>Page not found</h1>`)
+    res.status(404).send(`<h1>Page not found</h1>`)
 })
 
 app.listen(3000, () => console.log(`Server stared at port : 3000`));
