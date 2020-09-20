423eae7a6b1612f8ecbe53c13da6a2782b81cde4
vishnu <chilamakurvishnu@gmail.com>
2020-09-20 12:33:34 +0530

    setting up project

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// requiring express module
 const express = require("express");
+
+// requiring body parser
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function (req, res, next) {
-  res.send("<h1>A new shopping app</h1>");
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello</h1>");
 });
 
 app.listen(3000, function () {
