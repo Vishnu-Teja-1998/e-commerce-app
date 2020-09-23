918d30b6b77e6832013bb66782d3dd7f66cf2de7
Vishnu-Teja-1998 <63787817+Vishnu-Teja-1998@users.noreply.github.com>
2020-09-23 11:08:04 +0530

    passing data with post requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,6 +39,12 @@ exports.getCart = (req, res, next) => {
     });
 };
 
+exports.postCart = (req, res, next) => {
+    const productId = req.body.productId;
+    console.log(productId)
+    res.redirect("/cart")
+}
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
