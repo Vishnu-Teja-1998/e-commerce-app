be2ad8305f91eec18a1328ca5a603cb3e2bc1d10
Vishnu-Teja-1998 <63787817+Vishnu-Teja-1998@users.noreply.github.com>
2020-09-23 09:27:51 +0530

    styles opted for mobile screen and fixed some issues

--- app.js
+++ app.js
@@ -5,19 +5,13 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-<<<<<<< HEAD
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/404")
-=======
->>>>>>> enhancing_the_app
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -27,11 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-<<<<<<< HEAD
-app.use(errorController.getError);
-=======
 app.use(errorController.get404);
->>>>>>> enhancing_the_app
 
 app.listen(3000, function () {
   console.log(`server started on port : 3000`);
