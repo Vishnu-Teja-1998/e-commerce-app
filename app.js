af2a29c4da3910a5c4730d98a52441bd08c55400
Vishnu-Teja-1998 <63787817+Vishnu-Teja-1998@users.noreply.github.com>
2020-09-25 16:33:31 +0530

    confeguring cookies

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require("./models/user")
+const User = require('./models/user');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-mongoose.set("useCreateIndex", true);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -29,7 +28,7 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
-})
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,23 +36,26 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(
-  "mongodb+srv://admin-vishnu:vishnu123@vishnu-1nuon.mongodb.net/shopDB", {
+mongoose
+  .connect("mongodb+srv://admin-vishnu:vishnu123@vishnu-1nuon.mongodb.net/shopDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-).then(result => {
-  User.findOne().then(user => {
-    if (!user) {
-      const user = new User({
-        name: "Visshnnu",
-        email: "chilamakurvishnu@gmail.com",
-        cart: {
-          items: []
-        }
-      })
-      user.save();
-    }
   })
-  app.listen(3000, () => console.log(`server started on port 3000`));
-}).catch(err => console.log(err));
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Visshnnu',
+          email: 'chilamakurvishnu@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000, () => console.log(`server started on port 3000`));
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,17 @@
 exports.getLogin = (req, res, next) => {
     const isLoggedIn = req
-        .get("Cookie")
-        .split(";")[1]
+        .get('Cookie')
+        .split(';')[1]
         .trim()
-        .split("=")[1] === "true";
-    console.log(isLoggedIn);
-    res.render("./auth/login", {
-        path: "/login",
-        pageTitle: "Login",
+        .split('=')[1];
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
         isAuthenticated: isLoggedIn
-    })
-}
+    });
+};
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader("Set-Cookie", "loggedIn=true")
-    res.redirect("/")
-}
+    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.redirect('/');
+};
