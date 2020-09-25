54354728c6910852abfd71afe3668f6738d22d3e
Vishnu-Teja-1998 <63787817+Vishnu-Teja-1998@users.noreply.github.com>
2020-09-25 10:52:17 +0530

    adding and using a user model

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose")
 
 const errorController = require('./controllers/error');
+const User = require("./models/user")
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -13,12 +14,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-mongoose.connect(
-  "mongodb+srv://admin-vishnu:vishnu123@vishnu-1nuon.mongodb.net/shopDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
 mongoose.set("useCreateIndex", true);
 
 app.use(bodyParser.urlencoded({
@@ -26,11 +21,37 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('5f6d7cca2ec90357e820e03c')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000, () => {
-  console.log(`server started on port : 3000`);
-})
+mongoose.connect(
+  "mongodb+srv://admin-vishnu:vishnu123@vishnu-1nuon.mongodb.net/shopDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then(result => {
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name: "Visshnnu",
+        email: "chilamakurvishnu@gmail.com",
+        cart: {
+          items: []
+        }
+      })
+      user.save();
+    }
+  })
+  app.listen(3000, () => console.log(`server started on port 3000`));
+}).catch(err => console.log(err));
